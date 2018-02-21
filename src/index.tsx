26263c0c81c6b59c2d37a7700a1d91dddf308d4c
Efreeto <woowon.kim@gmail.com>
2018-02-20 22:27:25 -0800

    Add Hello component and style

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import Hello from './components/Hello';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Hello name="TypeScript" enthusiasmLevel={10} />,
   document.getElementById('root') as HTMLElement
 );
+/* To use type assertion put 'as HTMLElement' or '!' */
 registerServiceWorker();
